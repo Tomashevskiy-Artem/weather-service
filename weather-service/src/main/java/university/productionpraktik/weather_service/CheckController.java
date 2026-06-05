b930d8f524fa0e910004a0be77b2b2f7fbf2f601
Tomas <tomashtomash1@outlook.com>
2026-06-05 09:15:46 +0300

    Выполнен этап 1 без создания файлов env.example и docker-compose.yml. Ответ на Get \actuator\health выполняется

---
+++ weather-service/src/main/java/university/productionpraktik/weather_service/CheckController.java
@@ -0,0 +1,15 @@
+package university.productionpraktik.weather_service;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // обработка HTTP-запросов
+public class CheckController {
+    @GetMapping("/") // ответ клиента(браузера) от запроса GET на /(корневой путь приложения: http://localhost:8080/)
+    public String checkStatus() {
+        return "Weather-Service is working!!!";
+    }
+}
+
+// создал первый котроллер, который принимает HTTP-запрос GET
+// для проверки работоспособности сервера и связи с программой
